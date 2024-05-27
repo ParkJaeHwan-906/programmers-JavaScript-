635d462144bf0a4172046e6a9ec9af9331a92f63
Hwannee00 <doormoo2@naver.com>
2024-05-27 17:55:51 +0900

    Update no_120903.js

--- src/Level_0/no_120903.js
+++ src/Level_0/no_120903.js
@@ -1,3 +1,6 @@
+// 프로그래머스 Lv.0
+// 배열의 유사도
+// https://school.programmers.co.kr/learn/courses/30/lessons/120903
 function solution(s1, s2) {
     return s1.filter(e => {
         if(s2.includes(e)) return e;
